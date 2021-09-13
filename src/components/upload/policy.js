de708556f0da093818298ded252f755397b39c34
ex_wanggang1 <ex_wanggang1@axatp.com>
2021-09-13 09:22:33 +0800

    修改获取阿里云OSS验签时的类型参数传值方式

--- src/components/upload/policy.js
+++ src/components/upload/policy.js
@@ -1,9 +1,9 @@
 import http from '@/utils/httpRequest.js'
 
-export function policy(type) {
+export function policy(type = 'default') {
   return new Promise((resolve, reject) => {
     http({
-      url: http.adornUrl(`/api/upload/policy/${type}`),
+      url: http.adornUrl(`/api/upload/policy`),
       method: 'get',
       params: http.adornParams({ type: type })
     }).then(({ data }) => {
