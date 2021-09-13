6238b03ae8ca66f8c6886471d236cfd017b7c4f9
ex_wanggang1 <ex_wanggang1@axatp.com>
2021-09-13 09:17:30 +0800

    修改项目根路径，修改上传图片验签请求地址

--- src/components/upload/policy.js
+++ src/components/upload/policy.js
@@ -3,7 +3,7 @@ import http from '@/utils/httpRequest.js'
 export function policy(type) {
   return new Promise((resolve, reject) => {
     http({
-      url: http.adornUrl('/thirdparty/oss/policy'),
+      url: http.adornUrl(`/api/upload/policy/${type}`),
       method: 'get',
       params: http.adornParams({ type: type })
     }).then(({ data }) => {
